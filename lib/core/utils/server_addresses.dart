e215006f5c747f0845d26ccf345a9aaf5f1f389b
AMIRKHADHER <35258931+AMIRKHADHER@users.noreply.github.com>
2023-02-01 15:38:59 +0100

    Update server_addresses.dart
    edit ServerAddresses

--- lib/core/utils/server_addresses.dart
+++ lib/core/utils/server_addresses.dart
@@ -1,11 +1,4 @@
 class ServerAddresses {
-  //static const serverAddress = 'http://192.168.0.104:8443/api/';
-  // static const serverAddress = 'https://production.idtservices.fr:8443/idtsback-0.0.1-SNAPSHOT/';
-
-  //dev
-  // static const serverAddress = 'http://207.180.211.157:8080/idtsback-0.0.1-SNAPSHOT/';
-  //
-  // prod
-   static const serverAddress = 'https://production.idtservices.fr:8443/idtsback-0.0.1-SNAPSHOT/';
+  
 
 }
